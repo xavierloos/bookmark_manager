8d08ab19cd811bb2765f2d8a61d4ad3eb8fcc777
xavierloos <xavier.loos04@gmail.com>
2020-12-18 12:10:12 +0000

    CRUD

--- app.rb
+++ app.rb
@@ -2,25 +2,21 @@ require "sinatra/base"
 require "./lib/bookmark"
 
 class BookmarkManager < Sinatra::Base
-  enable :sessions
   get "/" do
-    p params
     erb :index
   end
 
   get "/bookmarks/new" do
-    p params
     erb :"bookmarks/new"
   end
 
   post "/bookmarks" do
-    p params
-    p Bookmark.create(url: params[:url], title: params[:title])
+    Bookmark.create(url: params[:url], title: params[:title])
     redirect "/bookmarks"
   end
 
   get "/bookmarks" do
-    p @bookmarks = Bookmark.all
+    @bookmarks = Bookmark.all
     erb :bookmarks
   end
 
@@ -29,9 +25,18 @@ class BookmarkManager < Sinatra::Base
   end
 
   post "/bookmarks/delete" do
-    p params
     Bookmark.delete(title: params[:title])
     redirect "/bookmarks"
+  end
+
+  get "/bookmarks/:id/edit" do
+    p @bookmark = Bookmark.find(id: params[:id])
+    erb :'bookmarks/edit'
+  end
+
+  post '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
   end
 
   run! if app_file == $0
